00cc8891a6409531629b8768e229d7124c99c2dc
Shani Shashidharan <46995046+logicmultimedia@users.noreply.github.com>
2024-06-06 14:20:41 +0530

    Homepage section design updates
    Homepage section design updated.

--- js/snct-scripts.js
+++ js/snct-scripts.js
@@ -1,8 +1,50 @@
 // SNCT JavaScript Document
-$(function(){
-    $("main").click(function(e){
-        if ($(".collapse.show").is(":visible")) {
-            $('.collapse').collapse('hide');	
-        }
+$(function () {
+  $("main").click(function () {
+    if ($(".collapse.show").is(":visible")) {
+      $('.collapse').collapse('hide');
+    }
+  });
+  if ($(".snct_slider").length > 0) {
+    $.getScript("js/slick.js").done(function () {
+      $(".snct_slider").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: false,
+        infinite: true,
+        centerMode: true,
+        nextArrow: '<i class="fa-solid fa-chevron-right"></i>',
+        prevArrow: '<i class="fa-solid fa-chevron-left"></i>',
+        responsive: [{
+            breakpoint: 1023,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1,
+              arrows: false,
+              autoplay: true,
+              autoplaySpeed: 2000,
+            }
+          },
+          {
+            breakpoint: 425,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              arrows: false,
+              autoplay: true,
+              autoplaySpeed: 2000,
+            }
+          }
+        ]
+      });
     });
+  }
+  if ($(".vmarquee").length > 0) {
+    $.getScript("js/jquery.marquee.js").done(function () {
+      $('.vmarquee').marquee({
+        direction: 'up',
+        speed: 30
+      });
+    });
+  }
 });

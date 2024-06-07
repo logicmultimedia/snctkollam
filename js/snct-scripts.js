f0edb5d7fb5309624a221f305d232e4d8c0b7b9d
Shani Shashidharan <46995046+logicmultimedia@users.noreply.github.com>
2024-06-07 18:22:15 +0530

    New management Webpage and responsive correction updates
    New management webpage and responsive correction updated.

--- js/snct-scripts.js
+++ js/snct-scripts.js
@@ -27,7 +27,7 @@ $(function () {
             }
           },
           {
-            breakpoint: 425,
+            breakpoint: 426,
             settings: {
               slidesToShow: 1,
               slidesToScroll: 1,
